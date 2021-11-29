900ce944c47fde1c568c946f831aeec496f94fc5
JordanMills233 <jordanmills233@gmail.com>
2021-11-29 14:57:15 +0000

    Implemented transaction list

---
+++ CoreBusiness/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBusiness
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } // if product name changes at any point
+        public double Price { get; set; }
+        public int BeforeQty { get; set; }
+        public int SoldQty { get; set; }
+        public string CashierName { get; set; }
+    }
+}
